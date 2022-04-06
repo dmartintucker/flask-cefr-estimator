6b58eb3151d037d9e572a88b638e2292346626ce
tuckerdm <dmartintucker@gmail.com>
2022-04-06 10:57:49 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from livereload import Server
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+#    app.run()
+    server = Server(app.wsgi_app)
+    server.serve()
